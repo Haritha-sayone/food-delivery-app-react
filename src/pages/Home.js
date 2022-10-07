3909714e4a12c183ed2bd78d71fc857c55fbe00a
Haritha-sayone <haritha@sayonetech.com>
2022-10-07 12:55:14 +0530

    show signIn/signUp in the Home page only when the user is logged out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,23 +4,27 @@ import Image from '../images/parcel.jpg';
 import About from "./About";
 import Menu from "./Menu";
 import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../context/UserAuthContext";
 
 
 function Home() {
     const navigate = useNavigate();
+    const { loggedUser } = useUserAuth();
     return (
         <div className="home">
             <img src={Image} width={"1294px"} height={"510px"} alt="pic of parcel" />
             <About />
-            <div className="d-grid gap-3 col-6 mx-auto" style={{ marginBottom: "60px" }}>
-                <button className="btn btn-warning" type="button" onClick={() => {
-                    navigate("./signup")
-                }}>SignUp</button>
+            {
+                !loggedUser && <div className="d-grid gap-3 col-6 mx-auto" style={{ marginBottom: "60px" }}>
+                    <button className="btn btn-warning" type="button" onClick={() => {
+                        navigate("./signup")
+                    }}>SignUp</button>
 
-                <button className="btn btn-warning" type="button" onClick={() => {
-                    navigate("./signin")
-                }}>SignIn</button>
-            </div>
+                    <button className="btn btn-warning" type="button" onClick={() => {
+                        navigate("./signin")
+                    }}>SignIn</button>
+                </div>
+            }
             <Menu />
         </div>
     )
