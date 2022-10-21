6f98f1e3437b100abf980db86b277fcf5bd52ff0
Haritha-sayone <haritha@sayonetech.com>
2022-10-21 11:20:37 +0530

    moved firebase configurations to .env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAZ18O82rw0H97oGGj3JCO99M5nCqfPEbA",
-    authDomain: "foodapp-e3bf7.firebaseapp.com",
-    databaseURL: "https://foodapp-e3bf7-default-rtdb.firebaseio.com",
-    projectId: "foodapp-e3bf7",
-    storageBucket: "foodapp-e3bf7.appspot.com",
-    messagingSenderId: "553238965376",
-    appId: "1:553238965376:web:58bd95b5264252bf310a65"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // init firebase app
@@ -24,7 +23,6 @@ const storage = getStorage(app);
 const usersRef = collection(db, "users");
 const itemsRef = collection(db, "items");
 const cartRef = collection(db, "cart");
-
 
 export default app;
 export {
